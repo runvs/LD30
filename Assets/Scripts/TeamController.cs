c7365aa6fe7d9bc476b1eb285b8e6f0a643304ab
Thunraz <julian.dinges@gmail.com>
2014-08-23 17:34:41 +0200

    Team member selection has been implemented

---
+++ Assets/Scripts/TeamController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamController : MonoBehaviour
+{
+    public static List<string> SelectedTeamMembers = new List<string>();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
