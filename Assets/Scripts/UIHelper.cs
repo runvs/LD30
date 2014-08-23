6c2419934609bb280e1b6e72ba31fa002d32fcbe
Thunraz <julian.dinges@gmail.com>
2014-08-23 14:06:03 +0200

    Added some UI stuff

---
+++ Assets/Scripts/UIHelper.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class UIHelper : MonoBehaviour
+{
+    public void ShowUiForRoom(string roomType)
+    {
+        var canvases = GameObject.FindGameObjectsWithTag("Canvases");
+
+        foreach (var canvas in canvases)
+        {
+            if (canvas.name == roomType + "Canvas")
+            {
+                var canvasComponent = canvas.GetComponent<Canvas>();
+                canvasComponent.enabled = true;
+            }
+            else if (canvas.name != "WorldSpaceCanvas")
+            {
+                var canvasComponent = canvas.GetComponent<Canvas>();
+                canvasComponent.enabled = false;
+            }
+        }
+    }
+}
