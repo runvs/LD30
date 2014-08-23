95999b409f6c4606f52b3b0d8336ab8240e404a2
Thunraz <julian.dinges@gmail.com>
2014-08-23 19:28:09 +0200

    Team selection should be finished for now.

--- Assets/Scripts/TeamSelection.cs
+++ Assets/Scripts/TeamSelection.cs
@@ -4,11 +4,27 @@ using UnityEngine.UI;
 public class TeamSelection : MonoBehaviour
 {
     public string TeamMemberName;
+    public int Tier;
 
     void Start()
     {
         var toggle = GetComponent<Toggle>();
         toggle.onValueChanged.AddListener(CheckTeamMembers);
+
+        if (Tier == 2)
+        {
+            if (!GameController.Tier2Available)
+            {
+                toggle.interactable = false;
+            }
+        }
+        else if (Tier == 3)
+        {
+            if (!GameController.Tier3Available)
+            {
+                toggle.interactable = false;
+            }
+        }
     }
 
     public void CheckTeamMembers(bool added)
