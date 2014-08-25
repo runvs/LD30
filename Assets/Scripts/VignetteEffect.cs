4bfec329a256cb7ffe044ba4d9745ba5b855171c
Laguna <laguna_999@web.de>
2014-08-25 17:31:23 +0200

    adding VignetteEffect

--- Assets/Scripts/VignetteEffect.cs
+++ Assets/Scripts/VignetteEffect.cs
@@ -8,15 +8,16 @@ public class VignetteEffect : MonoBehaviour
 
     private Texture2D VignetteEffectTexture;
     private Sprite VignetteEffectSprite;
+    Vector2 centerPosition;
 
     // Use this for initialization
     void Start()
     {
         Resolution = new Vector2(Screen.width, Screen.height);
+        centerPosition = new Vector2(Resolution.x / 2.0f, Resolution.y / 2.0f);
         CreateTexture();
-        VignetteEffectSprite = Sprite.Create(VignetteEffectTexture, new Rect(0, 0, Resolution.x, Resolution.y), new Vector2(Resolution.x / 2.0f, Resolution.y / 2.0f));
-        //VignetteEffectSprite.
-        GetComponent<SpriteRenderer>().sprite = VignetteEffectSprite;
+        GetComponent<GUITexture>().texture = VignetteEffectTexture;
+        GetComponent<GUITexture>().pixelInset = new Rect(0, 0, centerPosition.x, centerPosition.y);
     }
 
     private void CreateTexture()
@@ -24,23 +25,20 @@ public class VignetteEffect : MonoBehaviour
         col = Color.black;
         VignetteEffectTexture = new Texture2D((int)Resolution.x, (int)Resolution.y);
 
-        Vector2 centerPosition = new Vector2(Resolution.x / 2.0f, Resolution.y / 2.0f);
+
 
         float distanceToCenterMax = Mathf.Sqrt(centerPosition.x * centerPosition.x + centerPosition.y * centerPosition.y);
 
-        for (int i = 0; i <= centerPosition.x; i++)
+        for (int i = 0; i <= Resolution.x; i++)
         {
-            for (int j = 0; j <= centerPosition.y; j++)
+            for (int j = 0; j <= Resolution.y; j++)
             {
                 Color pixelCol = col;
 
                 Vector2 distanceToCenter = new Vector2(centerPosition.x - i, centerPosition.y - j);
 
 
-                //newCol.A = (byte)newAlpha;
-                //fadeRadialImage.SetPixel(i, j, newCol);
-
-                float newAlpha = 255.0f * 0.75f * (float)Mathf.Sqrt(distanceToCenter.x * distanceToCenter.x + distanceToCenter.y * distanceToCenter.y) / distanceToCenterMax;
+                float newAlpha = 0.65f * (float)Mathf.Sqrt(distanceToCenter.x * distanceToCenter.x + distanceToCenter.y * distanceToCenter.y) / distanceToCenterMax;
 
                 if (newAlpha < 0.0f)
                 {
